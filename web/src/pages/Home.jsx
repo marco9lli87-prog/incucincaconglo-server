146b82f214871dfcfb5e66cc88c3a2688180768e
marco9lli87-prog <marco9lli87@gmail.com>
2025-12-13 15:22:44 +0100

    Update Home.jsx

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 export default function Home () {
   return  (
-    <div style={{ color: "red" padding"40px" }}>
+    <div style={{ color: "red", padding"40px" }}>
       <h1>Incucina con Glò</h1>
         <p>Se vedi va</p>
     </div>
