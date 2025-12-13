bb210914cee6a7c862efef65f1a7a758324d4143
marco9lli87-prog <marco9lli87@gmail.com>
2025-12-13 14:50:24 +0100

    Update Home.jsx

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -1,44 +1,8 @@
-import { useEffect, useState } from "react";
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-);
-
-export default function Home() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    async function loadProducts() {
-      const { data, error } = await supabase
-        .from("products")
-        .select("*")
-        .eq("active", true);
-
-      if (error) {
-        console.error("Errore prodotti:", error);
-      } else {
-        setProducts(data);
-      }
-    }
-
-    loadProducts();
-  }, []);
-
-  return (
-    <div>
+export default function Home () {
+  return  (
+    <div style={{ padding: "20px", color: "white" }}>
       <h1>In Cucina con Glò</h1>
-
-      {products.length === 0 && <p>Nessun prodotto trovato</p>}
-
-      <ul>
-        {products.map((p) => (
-          <li key={p.id}>
-            <strong>{p.name}</strong> – {p.price} € / {p.unit}
-          </li>
-        ))}
-      </ul>
+        <p>Se vedi questo testo, React funziona</p>
     </div>
-  );
-}
+};
+      
