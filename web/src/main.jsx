9576e58130ab277eaeaa692909f3c778f7dd7ee8
marco9lli87-prog <marco9lli87@gmail.com>
2025-12-13 14:52:50 +0100

    Update main.jsx

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<React.StrictMode> <App /> </React.StrictMode>);
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode> 
+    <App /> 
+  </React.StrictMode>
+);
 
