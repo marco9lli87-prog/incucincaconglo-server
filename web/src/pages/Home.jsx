f30198d5ef37e6db45147a7f8cec3f93f2054e0f
marco9lli87-prog <marco9lli87@gmail.com>
2025-12-13 15:10:28 +0100

    Update Home.jsx

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 export default function Home () {
   return  (
-    <div style={{ padding: "20px", color: "white" }}>
+    <div style={{ padding: '20px', color: 'white' }}>
       <h1>In Cucina con Glò</h1>
         <p>Se vedi questo testo, React funziona</p>
     </div>
