3c957e80b818dfb5e48d5ad42fe37c732310c55d
marco9lli87-prog <marco9lli87@gmail.com>
2025-12-13 14:06:51 +0100

    Update main.jsx

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,1 +1,5 @@
-console.log('frontend')
+import React from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App"
+
+createRoot(document.getElementById("root)).render(<React.StrictMode> <App /> </React.StrictMode>)
