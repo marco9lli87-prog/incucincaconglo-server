5dfb74a9ad045be72d848addd0f472964b099ebc
marco9lli87-prog <marco9lli87@gmail.com>
2025-12-14 16:55:44 +0100

    Update main.jsx

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './styles.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles.css";
 
-createRoot(document.getElementById('root')).render(
+const root = document.getElementById("root");
+
+createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
-
+);
